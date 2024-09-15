da0bbda843c84bb489917649500ea761c67a3a44
kadukrunal <krunalkadu.01@gmail.com>
2024-09-15 17:45:58 +0530

    Initial Commit

---
+++ src/main/java/MainAppCode.java
@@ -0,0 +1,9 @@
+
+public class MainAppCode {
+	public static void main(String[] args) {
+		System.out.println("Welcome to Test Application!!!");
+		System.out.println("This is a demo application to test CICD with jenkins and maven");
+		System.out.println("Main method");
+	}
+	
+}
