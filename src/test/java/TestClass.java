9379db6a16a09cb70766b924cb05a3e733c5301e
kadukrunal <krunalkadu.01@gmail.com>
2024-09-17 15:46:06 +0530

    Added test package

---
+++ src/test/java/TestClass.java
@@ -0,0 +1,15 @@
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class TestClass {
+
+	@Test
+	public void testM1() {
+		Assert.assertTrue(true);
+	}
+
+	@Test
+	public void testM2() {
+		Assert.assertTrue(true);
+	}
+}
