12ed8d1b764c0718caf10dd65896922fc34d362f
kadukrunal <krunalkadu.01@gmail.com>
2024-09-17 17:26:03 +0530

    Updated main class

--- src/main/java/MainAppCode.java
+++ src/main/java/MainAppCode.java
@@ -3,7 +3,7 @@ public class MainAppCode {
 	public static void main(String[] args) {
 		System.out.println("*********Welcome to Test Application!!!***********");
 		System.out.println("This is a demo application to test CICD with jenkins and maven");
-		System.out.println("Main method");
+		System.out.println("Main method of Application");
 	}
 	
 }
